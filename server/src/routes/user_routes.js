9373f5c3cafb561d9a5d7be55021f0c93872d323
devonbarangan <barangandevon@gmail.com>
2026-01-26 20:26:12 +0800

    chore: added admin dashboard view

--- server/src/routes/user_routes.js
+++ server/src/routes/user_routes.js
@@ -2,12 +2,25 @@ import express from "express";
 import { protectRoute } from "../middlewares/auth_middleware.js";
 import { authorizedRoles } from "../middlewares/auth_roles.js";
 import { getUserInfo } from "../controllers/user_controller.js";
+import {
+  getAllResident,
+  totalAnnouncement,
+  totalCertificateRequest,
+} from "../controllers/admin_controller.js";
 
 const router = express.Router();
 
-router.get("/admin", protectRoute, authorizedRoles("Admin"), (req, res) => {
-  res.send("Welcome Admin");
-});
+router.get("/admin", protectRoute, authorizedRoles("Admin"), getAllResident);
+
+router.get(
+  "/admin/total",
+  protectRoute,
+  authorizedRoles("Admin"),
+  totalCertificateRequest,
+);
+
+router.get("/total/announcement", totalAnnouncement);
+
 router.get(
   "/staff",
   protectRoute,
