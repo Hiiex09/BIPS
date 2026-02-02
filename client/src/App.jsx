874a7faff54b96f707a6709854942efbf63b6765
devonbarangan <barangandevon@gmail.com>
2026-02-02 20:19:19 +0800

    chore: modified app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,22 @@ import AdminLandingPage from "./pages/AdminUI/AdminLandingPage";
 import Login from "./pages/Login.jsx";
 import Signup from "./pages/Signup.jsx";
 
+import AlertBar from "./components/AlertBar.jsx";
+import Navbar from "./components/Navbar.jsx";
+import LandingPage from "./components/LandingPage.jsx";
 const App = () => {
   return (
     <>
       {/* <AdminLandingPage /> */}
 
       <BrowserRouter>
+        <Navbar />
+        <AlertBar />
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/admin" element={<AdminLandingPage />} />
+          <Route path="/" element={<LandingPage />} />
         </Routes>
       </BrowserRouter>
     </>
