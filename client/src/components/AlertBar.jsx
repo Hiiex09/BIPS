6a59d07466cea558ef0b488cd52f795a511c1ecd
devonbarangan <barangandevon@gmail.com>
2026-02-10 20:10:46 +0800

    chore: made some modified components

--- client/src/components/AlertBar.jsx
+++ client/src/components/AlertBar.jsx
@@ -1,16 +1,28 @@
 import { Megaphone } from "lucide-react";
+import { getAnnouncement } from "../hooks/UseAnnouncementRouteHooks";
 
 const AlertBar = () => {
+  const { data, isLoading, error } = getAnnouncement();
+
+  if (isLoading) {
+    return <p>Loading please wait</p>;
+  }
+
+  if (error) {
+    return <p>Error no data</p>;
+  }
+
+  console.log(data);
+
   return (
     <div className="w-screen">
       <div className="alert bg-blue-300 shadow flex justify-start">
         <Megaphone size={20} />
-        <span>Vaccination Drive this Saturday – 8:00 AM</span>
-        <span>Vaccination Drive this Saturday – 8:00 AM</span>
-        <span>Vaccination Drive this Saturday – 8:00 AM</span>
-        <span>Vaccination Drive this Saturday – 8:00 AM</span>
-        <span>Vaccination Drive this Saturday – 8:00 AM</span>
-        <span>Vaccination Drive this Saturday – 8:00 AM</span>
+        {data.map((d) => (
+          <div key={d._id}>
+            <h2>{d.title}</h2>
+          </div>
+        ))}
         <button className="btn btn-sm btn-primary">Details</button>
       </div>
     </div>
--- client/src/components/LatestUpdate.jsx
+++ client/src/components/LatestUpdate.jsx
@@ -1,15 +1,33 @@
+import { getAnnouncement } from "../hooks/UseAnnouncementRouteHooks";
+
 export const LatestUpdate = () => {
+  const { data = [], isLoading, error } = getAnnouncement();
+
   return (
     <div className="lg:col-span-8 mx-2 text-2xl font-semibold py-4">
       <h3 className="font-bold mb-4">Latest Updates</h3>
       <div className="bg-base-200 rounded-2xl p-6">
         <div className="space-y-4 text-sm">
-          <div className="p-3 bg-base-100 rounded-lg">
-            Community Clean-up Drive Success
-          </div>
-          <div className="p-3 bg-base-100 rounded-lg">
-            New Health Center Operating Hours
-          </div>
+          {data.map((d) => (
+            <div className="bg-base-300 rounded-2xl" key={d._id}>
+              <div className="p-3 rounded-lg">
+                <span
+                  className={`badge badge-sm ${
+                    d.priority === "Normal"
+                      ? "badge badge-primary"
+                      : d.priority === "Important"
+                        ? "badge badge-secondary"
+                        : "badge badge-error"
+                  } text-xs float-end`}
+                >
+                  {d.priority}
+                </span>
+                <h2 className="text-lg">{d.title}</h2>
+                <p className="">{d.content}</p>
+                <p className="text-xs"> {d.author.fullname}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
