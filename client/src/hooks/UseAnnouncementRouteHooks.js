ff43c9390f63447c9051af71521df7ef49b6533f
devonbarangan <barangandevon@gmail.com>
2026-02-10 20:11:04 +0800

    chore: made own hoon for announcement

--- client/src/hooks/UseAnnouncementRouteHooks.js
+++ client/src/hooks/UseAnnouncementRouteHooks.js
@@ -1,5 +1,8 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { createAnnouncementApi } from "../api/announcement_api";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import {
+  createAnnouncementApi,
+  getAnnouncementApi,
+} from "../api/announcement_api.js";
 
 export const createAnnouncement = () => {
   const queryClient = useQueryClient();
@@ -8,10 +11,19 @@ export const createAnnouncement = () => {
     mutationFn: createAnnouncementApi,
     onSuccess: () => {
       console.log("New announcement created");
-      queryClient.invalidateQueries({ queryKey: ["announcement"] });
+      queryClient.invalidateQueries({ queryKey: ["announcements"] });
     },
     onError: (err) => {
       console.error("Create failed:", err);
     },
   });
+};
+
+export const getAnnouncement = () => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["announcements"],
+    queryFn: getAnnouncementApi,
+  });
+
+  return { data, isLoading, error };
 };
