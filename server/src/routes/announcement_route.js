48457b6dbe0ed0a06037bf76f533d261228d48d5
DevonJing <barangandevon@gmail.com>
2026-02-26 20:10:23 +0800

    chore: modified routes

--- server/src/routes/announcement_route.js
+++ server/src/routes/announcement_route.js
@@ -7,7 +7,7 @@ import {
 import { protectRoute } from "../middlewares/auth_middleware.js";
 import { authorizedRoles } from "../middlewares/auth_roles.js";
 import { validate } from "../middlewares/validate.js";
-import { createAnnouncementSchema } from "../middlewares/announcement_validation.js";
+import { createAnnouncementSchema } from "../validator/announcement_validation.js";
 
 const router = express.Router();
 
