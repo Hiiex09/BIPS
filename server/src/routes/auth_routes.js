df8a1bd7701e608c0b70de7c869759242f065b75
barangandevon <barangandevon@gmail.com>
2026-03-07 21:43:53 +0800

    chore: added validation for inputs

--- server/src/routes/auth_routes.js
+++ server/src/routes/auth_routes.js
@@ -7,11 +7,18 @@ import {
 } from "../controllers/auth_controller.js";
 import { protectRoute } from "../middlewares/auth_middleware.js";
 import upload from "../middlewares/upload.js";
+import { validate } from "../middlewares/validate.js";
+import { loginSchema, signupSchema } from "../validator/auth_validation.js";
 
 const router = express.Router();
 
-router.post("/signup", upload.single("idUpload"), signup);
-router.post("/login", login);
+router.post(
+  "/signup",
+  upload.single("idUpload"),
+  validate(signupSchema),
+  signup,
+);
+router.post("/login", validate(loginSchema), login);
 router.post("/logout", logout);
 router.get("/checkAuth", protectRoute, checkAuth);
 
